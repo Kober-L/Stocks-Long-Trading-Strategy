dbc13189c44cbf26a18f4b6f95735639e87690cb
Kober-L <lollo.voro@gmail.com>
2022-03-25 09:31:12 +0100

    Building detect trend function, created Heikin ashi function

--- first_analysis_indicators.py
+++ first_analysis_indicators.py
@@ -10,7 +10,25 @@ def detect_trend(prices, points = 8, candles = 104, growth = 5.7, long = True):
         -candles: the evaluation time of the trend: 104 candles = 2 years
         -growth: the percentage of growth between two points
         -long: determine if we want to detect a long or short trend
+
+    This functions check at prices, taking n points that are prices and veryfies if there is enough growth between them
     '''
+
+    #be sure you are considering only the interested period (the correct amount of candeles)
+    prices = prices.tail(candles)
+    i = 0
+    points_df = pd.DataFrame()
+    for index, row in prices.iterrows():
+        if i % 13 == 0:
+            
+        else:
+            pass
+
+        i += 1
+#continue from this point
+        
+
+#dopo metti MA(questi devono ritornare solo la colonna adj close!), EMA, slope indicators
 
     #extract the prices
     #select the points
--- utils.py
+++ utils.py
@@ -8,9 +8,10 @@ import matplotlib as plt
 from datetime import datetime
 import plotly.graph_objects as go
 import yfinance as yf
+import investpy
 
 
-def plot_df(df):
+def plot_OHLC(df):
 
     fig = go.Figure(data=[go.Candlestick(x=df.index,
                 open=df['Open'],
@@ -21,7 +22,33 @@ def plot_df(df):
     fig.show()
 
 
-df = yf.download(tickers='AAPL', period='1y', interval='1wk')
+def OHLC_to_HA(df):
+    df_ha = df.copy()
+    for i in range(df_ha.shape[0]):
+        if i > 0:
+            df_ha.loc[df_ha.index[i],'Open'] = (df['Open'][i-1] + df['Close'][i-1])/2
+    
+        df_ha.loc[df_ha.index[i],'Close'] = (df['Open'][i] + df['Close'][i] + df['Low'][i] +  df['High'][i])/4
+    df_ha = df_ha.iloc[1:,:]
+    return df_ha
 
-plot_df(df)
-print('done')
+
+#need to check at his failure
+def get_stock_hist_data(tycker, timeframe_ = '1w', period_ = '2y',
+                        country_ = 'United States', start_date = '01/01/2019', 
+                        end_date = '01/01/2022'): 
+    try:
+        df = yf.download(tickers=tycker, period=period_, interval=timeframe_)
+    
+    except:
+        df = investpy.get_stock_historical_data(stock=tycker,
+                                                country=country_,
+                                                from_date=start_date,
+                                                to_date=end_date)
+    return df
+
+
+
+
+
+
