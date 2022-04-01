4c442e6db4c8f447a031fe45fa45cf28dfbc7288
Kober-L <lollo.voro@gmail.com>
2022-04-01 09:57:44 +0200

    completed detect trend function, now defne first tests

--- first_analysis_indicators.py
+++ first_analysis_indicators.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-def detect_trend(prices, points = 8, candles = 104, growth = 5.7, long = True):
+def detect_trend(prices, points = 8, candles = 104, growth = 5.1, long = True):
     '''
     Input:
-        -prices: pandas dataframe with prices derived from a 50MA
+        -prices: pandas dataframe with prices derived from a 50MA --> a series that is adj close price 
         -points: the number of prices I want to check in orer to see if there is enough growth
         -candles: the evaluation time of the trend: 104 candles = 2 years
         -growth: the percentage of growth between two points
@@ -14,18 +14,37 @@ def detect_trend(prices, points = 8, candles = 104, growth = 5.7, long = True):
     This functions check at prices, taking n points that are prices and veryfies if there is enough growth between them
     '''
 
-    #be sure you are considering only the interested period (the correct amount of candeles)
+    #be sure you are considering only the interested period (the correct amount of candles)
     prices = prices.tail(candles)
     i = 0
-    points_df = pd.DataFrame()
-    for index, row in prices.iterrows():
+    points = []
+    for index, row in prices.items():
         if i % 13 == 0:
-            
+            points.append(row)
         else:
             pass
 
         i += 1
-#continue from this point
+    
+    #now that you extracted to consider prices, check th proper growth
+    #tasso di crescita rispetto al punto precedente o in generale??
+    for i in range(len(points) - 1):
+        start_price = points[i]
+        next_price = points[i + 1]
+        one_perc = start_price / 100
+        delta = next_price - start_price
+        perc_change = delta / one_perc
+        if not (perc_change >= growth):
+            return False
+        
+    return True
+
+
+
+
+
+
+
         
 
 #dopo metti MA(questi devono ritornare solo la colonna adj close!), EMA, slope indicators
---
+++ test_units.py
@@ -0,0 +1,8 @@
+import pytest
+import pandas as pd
+
+class TestFirst_anl_ind():
+    def test_detect_trend(self):
+        ascending_series =
+        stationary_series =
+        descending_series =
